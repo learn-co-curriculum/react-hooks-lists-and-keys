04cc6fe25c485dc55677787926fa247c5b036b6a
ihollander <ianholla@gmail.com>
2020-12-12 08:01:53 -0500

    Initial commit

---
+++ src/components/ColorList.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function ColorList() {
+  const colors = [
+    "firebrick",
+    "rebeccapurple",
+    "salmon",
+    "darkslategray",
+    "hotpink",
+  ];
+
+  return (
+    <div>
+      <h1>Top 5 CSS Colors</h1>
+      <li style={{ color: colors[0] }}>{colors[0]}</li>
+      <li style={{ color: colors[1] }}>{colors[1]}</li>
+      <li style={{ color: colors[2] }}>{colors[2]}</li>
+      <li style={{ color: colors[3] }}>{colors[3]}</li>
+      <li style={{ color: colors[4] }}>{colors[4]}</li>
+    </div>
+  );
+}
+
+export default ColorList;
