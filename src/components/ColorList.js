c22c2665a98cd0731676816167fa89876ed1ef98
Liz Burton <liz.burton147@gmail.com>
2021-08-10 13:21:56 -0400

    Update starter code

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -12,11 +12,13 @@ function ColorList() {
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
-      <li style={{ color: colors[0] }}>{colors[0]}</li>
-      <li style={{ color: colors[1] }}>{colors[1]}</li>
-      <li style={{ color: colors[2] }}>{colors[2]}</li>
-      <li style={{ color: colors[3] }}>{colors[3]}</li>
-      <li style={{ color: colors[4] }}>{colors[4]}</li>
+      <ol>
+        <li style={{ color: colors[0] }}>{colors[0]}</li>
+        <li style={{ color: colors[1] }}>{colors[1]}</li>
+        <li style={{ color: colors[2] }}>{colors[2]}</li>
+        <li style={{ color: colors[3] }}>{colors[3]}</li>
+        <li style={{ color: colors[4] }}>{colors[4]}</li>
+      </ol>
     </div>
   );
 }
